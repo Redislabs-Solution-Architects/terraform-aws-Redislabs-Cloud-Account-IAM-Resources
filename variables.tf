7b3451c65ab1a30dbf2453cd3459dada7ceeee6f
Toby H Ferguson <toby.h.ferguson@gmail.com>
2020-12-31 05:12:42 -0800

    Removed all backtick characters

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "pgp_key" {
     type = string
-    description = "(Required) Either a base-64 encoded PGP public key, or a [keybase](https://keybase.io/) username in the form `keybase:username`. This user must exist within [keybase](https://keybase.io/)"
+    description = "(Required) Either a base-64 encoded PGP public key, or a keybase (see https://keybase.io/) username in the form 'keybase:username'. This user must exist within keybase"
 }
